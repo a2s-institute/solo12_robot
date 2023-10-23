8b5feace879000e051b72a7a2a138fff7462d8ab
harleylara <harley.lara@outlook.com>
2023-10-23 22:40:21 +0200

    fix model spawn in gazebo

--- launch/test_solo12_description.launch.py
+++ launch/test_solo12_description.launch.py
@@ -1,4 +1,5 @@
 import os
+import xacro
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
@@ -13,29 +14,28 @@ def generate_launch_description():
     rviz_config_file = PathJoinSubstitution([FindPackageShare(package_name), "rviz", "solo12.rviz"])
     
     # Get URDF path
-    urdf_path = os.path.join(get_package_share_directory(package_name), 'urdf', 'solo12.urdf')
-    
-    with open(urdf_path, "r") as urdf:
-        robot_desc = urdf.read()
+    urdf_path = os.path.join(get_package_share_directory(package_name), 'urdf', 'solo12.urdf.xacro')
 
-    robot_description = {"robot_description": robot_desc}
+    robot_description = {"robot_description": xacro.process_file(urdf_path).toxml()}
 
     joint_state_publisher_node = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
     )
+
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        parameters=[robot_description],
+        parameters=[robot_description, {"use_sim_time": True}],
     )
+
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="log",
-        arguments=["-d", rviz_config_file],
+        arguments=["-d", rviz_config_file]
     )
 
     return LaunchDescription(
