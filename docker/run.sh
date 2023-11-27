9314e490b05f249a23a9883a936c9a54c87fdc20
harleylara <harley.lara@outlook.com>
2023-11-27 03:45:57 +0100

    add docker run script

---
+++ docker/run.sh
@@ -0,0 +1,6 @@
+sudo xhost +si:localuser:host
+
+sudo docker run -it --rm --network host -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+    -v /tmp/argus_socket:/tmp/argus_socket \
+    solo-image
