4f0d5060e49084db24309d5f2c2999679eced81d
Akhilan-xd <akhilanofficial00@gmail.com>
2024-02-09 13:37:00 +0100

    [UPDATED] the hpp of the solo_api

--- solo_api/include/solo_api.hpp
+++ solo_api/include/solo_api.hpp
@@ -1,20 +1,25 @@
 #ifndef SOLO_API_H
 #define SOLO_API_H
 
-class SOLO_API {
+// #include "master-board/sdk/master_board_sdk/include/master_board_sdk/master_board_interface.h"
+// #include "master-board/sdk/master_board_sdk/include/master_board_sdk/defines.h"
+
+class SOLO_API{
 public:
     // Constructor
-    SOLO_API();
+    SOLO_API(int *argc, char ***argv); //allowing both type of input
 
     // Destructor
     ~SOLO_API();
 
     // Member functions
-    void initialize();
-    void performAction();
+    int initialize();
+    int performAction();
 
     // Member variables
     int data;
+    // MasterBoardInterface robot_if(std::string if_name);
+
 
 private:
     // Private member variables
